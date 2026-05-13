3e00fb569a2f271792cde7b443c338eb44850478
lucaIacopino <lucaiac01@gmail.com>
2026-05-13 10:43:40 +0200

    feat: add basic TUI layout with header, footer and shortcuts

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from textual.app import App
+from textual.widgets import Footer, Header
+
+class AppHome(App):
+    BINDINGS = [(("q", "quit", "Esci"))]
+    
+    def compose(self):
+        yield Header()  
+        yield Footer() 
+
+
 def main():
-    ...
+    AppHome().run()
 
 
 if __name__== "__main__":
